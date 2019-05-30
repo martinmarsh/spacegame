12262c1db26f6dd9a8f5bee89bd98dc76ce24f3a
Mick-Cadac <m.s.latham@ntlworld.com>
2019-05-30 21:47:00 +0100

    New player object with bomb control

---
+++ player.py
@@ -0,0 +1,38 @@
+from config import W, H
+from weapon import Bomb
+import pyxel
+
+
+class Player():
+
+    def __init__(self):
+        self.player_x = W/2
+        self.player_y = H/3
+        self.bomb = None
+        self.bomb_dropped = False
+
+    def reset(self):
+        self.player_x = W/2
+        self.player_y = H/3
+
+    def draw(self):
+        pyxel.blt(self.player_x, self.player_y, 0, 32, 0, 16, 16)
+        if self.bomb:
+            self.bomb.draw()
+
+    def update(self):
+        self.player_move()
+
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            self.bomb = Bomb(self.player_x, self.player_y)
+            self.bomb_dropped = True
+
+        if self.bomb_dropped:
+            self.bomb.update()
+
+    def player_move(self):
+        if pyxel.btn(pyxel.KEY_UP):
+            self.player_y = self.player_y - 2
+
+        if pyxel.btn(pyxel.KEY_DOWN):
+            self.player_y = self.player_y + 2
