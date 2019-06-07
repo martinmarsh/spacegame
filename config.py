cd0d0b179ab7c8c7dc45f838f6c09ef29db14382
mmarsh <mmarsh@bink.com>
2019-06-07 18:15:09 +0100

    Config now has comments on key definitions.  Ideally we need to be able to use game pad to select items in scores list with up and down keys and a pointing icon.  May be X to select and Y to choose a new name or we could add the new name option at bottom of list

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import pyxel
 import math
 
+# These are the 4 large blue and light blue buttons on right hand side:
 GAMEPAD_1_A = 3002
 GAMEPAD_1_B = 3001
 GAMEPAD_1_X = 3003
 GAMEPAD_1_Y = 3000
+
+# These keys are on the top of the controller and marked l and R
 GAMEPAD_1_L = 3004
 GAMEPAD_1_R = 3005
 
+# 2 other keys on the top are marked l2 and r2 not yet found
+
+# These keys are marked the 4 direction key pad:
 GAMEPAD_1_UP = 3014
 GAMEPAD_1_RIGHT = 3015
 GAMEPAD_1_DOWN = 3016
